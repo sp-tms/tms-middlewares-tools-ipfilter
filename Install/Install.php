8dbf995230002e8284a5fef910b4a1444055f45a
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-08 23:57:02 +0530

    Initial Commit

---
+++ Install/Install.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Apps\Tms\Middlewares\IpFilter\Install;
+
+use System\Base\BasePackage;
+
+class Install extends BasePackage
+{
+    public function install()
+    {
+        return true;
+    }
+
+    public function uninstall()
+    {
+        return true;
+    }
+}
---
+++ IpFilter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Apps\Tms\Middlewares\IpFilter;
+
+use System\Base\BaseMiddleware;
+
+class IpFilter extends BaseMiddleware
+{
+    public function process($data)
+    {
+        //
+    }
+}
