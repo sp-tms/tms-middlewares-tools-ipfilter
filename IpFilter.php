e97eba9a0ba3bee3db670cab68729cf13b680fa8
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-27 00:24:59 +0530

    update

--- IpFilter.php
+++ IpFilter.php
@@ -8,6 +8,14 @@ class IpFilter extends BaseMiddleware
 {
     public function process($data)
     {
-        //
+        if ($this->access->ipFilter->checkList()) {
+            return true;
+        }
+
+        $this->response->setStatusCode(404);
+
+        $this->response->send();
+
+        exit;
     }
 }
